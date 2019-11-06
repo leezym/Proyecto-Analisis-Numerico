e5e885c6522c6e68d2c43f3038643ba745febb72
icaldasr <38504416+icaldasr@users.noreply.github.com>
2019-11-06 17:40:50 -0500

    Documentacion

--- obtenerParametros.py
+++ obtenerParametros.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 
 eng = matlab.engine.start_matlab()
-eng.desktop(nargout=0)
+eng.desktop(nargout=0) #  returns the number of output arguments specified for a function.
 px = eng.linspace(0.0,6.28,3) #Crea el array
 eng.workspace['mx'] = px #Lo carga en el workspace
 x = [2,4,6,7]
@@ -21,3 +21,7 @@ print(mpi2)
 
 #https://la.mathworks.com/help/matlab/apiref/matlab.engine.matlabengine-class.html
 #https://la.mathworks.com/help/matlab/matlab_external/use-matlab-arrays-in-python.html
+#https://la.mathworks.com/help/matlab/matlab_external/pass-data-to-matlab-from-python.html
+#https://la.mathworks.com/help/matlab/matlab_external/passing-data-to-python.html
+#https://la.mathworks.com/help/matlab/matlab_external/python-function-arguments.html
+#https://la.mathworks.com/help/matlab/apiref/matlab.engine.start_matlab.html
